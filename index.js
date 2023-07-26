0f00dc02af44855fed0530c639e590202f99b068
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-07-25 17:11:32 -0700

    Completed exercie 3.2 and 3.3; added .gitignore

--- index.js
+++ index.js
@@ -28,6 +28,23 @@ app.get("/api/persons", (request, response) => {
   response.json(entries);
 });
 
+app.get("/info", (request, response) => {
+  const phonebookSize = entries.length;
+  const date = new Date();
+  response.send(`<p>Phonebook has information for ${phonebookSize} people.</p>
+    <p>${date}</p>`);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = entries.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
