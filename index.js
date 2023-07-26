3a436a3cb0fd7d5548a4420b83c49ee625b0321c
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-07-25 18:01:59 -0700

    Completed 3.5, 3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let entries = [
   {
     id: 1,
@@ -23,6 +25,10 @@ let entries = [
     number: "39-23-6423122",
   },
 ];
+
+const generateRandomId = (max) => {
+  return Math.floor(Math.random() * max);
+};
 
 app.get("/api/persons", (request, response) => {
   response.json(entries);
@@ -49,6 +55,33 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   entries = entries.filter((person) => person.id !== id);
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log(body);
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  if (
+    entries.some(
+      (person) => person.name.toLowerCase() === body.name.toLowerCase()
+    )
+  ) {
+    return response.status(400).json({ error: "resource already exists" });
+  }
+
+  const person = {
+    id: generateRandomId(10000),
+    name: body.name,
+    number: body.number,
+  };
+
+  entries = entries.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
