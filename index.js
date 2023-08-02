d76c4e7eacc494d9c8206fc8f63a946271ee5506
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-08-01 21:31:27 -0700

    Added error handling middleware for failed promises; Completed 3.16

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+
 const Person = require("./models/person");
 
 app.use(express.json());
@@ -17,32 +18,15 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body ")
 );
 
-let entries = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const generateRandomId = (max) => {
-  return Math.floor(Math.random() * max);
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformatted ID" });
+  }
+  next(error);
 };
+
+let entries = [];
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello world!</h1>");
@@ -61,18 +45,21 @@ app.get("/info", (request, response) => {
     <p>${date}</p>`);
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = entries.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        // Found a person
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  Person.findByIdAndDelete(request.params.id)
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
     .then((result) => {
       response.status(204).end();
     })
@@ -106,6 +93,8 @@ app.post("/api/persons", (request, response) => {
     response.json(savedPerson);
   });
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
