d7cb671d7b8b4348f9ae6a025ec6cbdd364ef4a7
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-08-01 21:07:06 -0700

    Delete hooked up to MongoDB; completed 3.15

--- index.js
+++ index.js
@@ -14,14 +14,7 @@ morgan.token("body", (req, res) => {
 });
 
 app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :body ",
-    {
-      skip: (req, res) => {
-        return req.method !== "POST";
-      },
-    }
-  )
+  morgan(":method :url :status :res[content-length] - :response-time ms :body ")
 );
 
 let entries = [
@@ -79,9 +72,11 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  entries = entries.filter((person) => person.id !== id);
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
